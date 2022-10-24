74944ff4af2a5dde79c9c40d6812775e69739aa6
Jin031 <Jin292941@gmail.com>
2022-10-24 12:08:22 +0000

    [Add] 5

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,12 @@ Rails.application.routes.draw do
  namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items
+    resources :customers, only: [:index, :show, :edit, :update]
+    root to: "homes#top"
  end
 
+ namespace :public do
+   resources :customers, only: [:show, :edit, :update, :withdraw, :unsubscribe]
+ end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
