132506c4bb618e3ba90cd58588505a6b11900f5c
Jin031 <Jin292941@gmail.com>
2022-11-09 11:42:44 +0000

    [Add] 5

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -16,15 +16,26 @@ class Public::OrdersController < ApplicationController
   end
 
   def complete
-
+    
   end
 
   def confirm
-
+    @cart_items =
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+   if params[:order][:select_address] ='0'
+    @order.postal_code =  current_customer.postal_code
+    @order.address =  current_customer.address
+    @order.name = current_customer.name
+   elsif params[:order][:select_address] ='1'　
+    
+   else 
+     
+   end
   end
 
   private
   def order_params
-      params.require(:order).permit(:customer_id, :postal_code, :address)
+      params.require(:order).permit(:customer_id, :postal_code, :address, :name, :total_payment, :status, :payment_method, :delivery_charge)
   end
 end
